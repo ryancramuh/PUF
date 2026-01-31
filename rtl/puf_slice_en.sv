ea800bcfa01de7d9c8a9fa26fe9d3409d2c82c4a
ryancramuh <ryancramer22@gmail.com>
2026-01-30 23:49:59 -0800

    Update puf_slice_en.sv

--- rtl/puf_slice_en.sv
+++ rtl/puf_slice_en.sv
@@ -1,23 +1,29 @@
 `timescale 1ns/1ps
 
+// avoids synthesizer optimizations
+(* dont_touch = "yes" *)
+
 module puf_slice_en(
-	input d0,
-	input d1,
-	input sel,
-	input en,
+	input d0,   // latch output
+	input d1,   // mux output
+	input sel,  // challenge[5]
+	input en,   // challenge[2]
 	input bx,
 	output logic o,
 	output logic l_o
 ); 
+
+	// keep ensure wires are not combined and optimized away
+	(* keep = "true" *) wire m1 = sel ? ~d1 : ~d0;
+	(* keep = "true" *) wire m2 = sel ? ~d1 : ~d0;
+	(* keep = "true" *) wire e1 = en ?  m1 :  1'b0; // these enable mux control the whole CRO
+	(* keep = "true" *) wire e2 = en ?  m2 :  1'b0; // these enable mux control the whole CRO 
+	(* keep = "true" *) wire b = bx ? e2 : e1;
 	
-	assign wire m1 = sel ? ~d1 : ~d0;
-	assign wire m2 = sel ? ~d1 : ~d0;
-	assign wire e1 = sel ?  m1 :  1'b0;
-	assign wire e2 = sel ?  m2 :  1'b0;
-	assign wire b  = bx ?  e2 :  e1;
+	assign o = b; // o is the mux output
 	
 	always_latch
 		if(en)
-			l_o <= b;
+			l_o <= b; // l_o is the latched version of the mux output
 
 endmodule
