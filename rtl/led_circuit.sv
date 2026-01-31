42e7d89f721fc30001c13cb83e6ccdd4595c2006
ryancramuh <ryancramer22@gmail.com>
2026-01-30 23:38:12 -0800

    Refactor led_circuit module for clarity and efficiency
    Refactor led_circuit module to improve readability and structure. Simplify counter logic and ensure proper signal handling.

--- rtl/led_circuit.sv
+++ rtl/led_circuit.sv
@@ -1,13 +1,19 @@
 `timescale 1ns/1ps
 
-module led_circuit(
-	input clk,
-	input [6:0] sw,
-	output logic [15:0] led
+// ensure synthesizer doens't touch this 
+(* dont_touch = "yes" *)
+
+module led_circuit
+(
+	input               clk, // global clock
+	input [6:0]         sw,  // Basys 3 switches
+	
+	output logic [15:0] led  // Basys 3 LEDs
 );
 
-	logic [19:0] cnt;
-	logic [19:0] cnt2;
+	logic        up;   // posedge of RO     
+	logic [19:0] cnt;  // clock divider counter
+	logic [19:0] cnt2; // RO posedge counter
 	
 	puf_cro cro(
 		.challenge(sw[5:0]),
@@ -16,19 +22,54 @@ module led_circuit(
 	);
 
 	always_ff@(posedge clk)
-		case(sw[6])
-			1'b0: cnt <= 0;
-			1'b1: cnt <= cnt + 1;
-		endcase
+		if((sw[6] == 1'b1))
+			cnt <= cnt + 1;
+		else
+			cnt <= 0;
 
-	always_ff@(posedge up);
-		case(sw[6])
-			1'b0: cnt2 <= 0;
-			1'b1: cnt2 <= cnt2 + 1;
-		endcase
+	always_ff@(posedge up)
+			cnt2 <= cnt2 + 1;
 
 	always_latch 
 		if(cnt == 20'hFFFFF)
+			led <= cnt2[15:0];
+	
+endmodule
+`timescale 1ns/1ps
+
+// ensure synthesizer doens't touch this 
+(* dont_touch = "yes" *)
+
+module led_circuit
+(
+	input               clk, // global clock
+	input [6:0]         sw,  // Basys 3 switches
+	
+	output logic [15:0] led  // Basys 3 LEDs
+);
+
+	logic        up;   // posedge of RO     
+	logic [19:0] cnt;  // clock divider counter
+	logic [19:0] cnt2; // RO posedge counter
+
+	// instantiate the CRO (now as a hard macro pblock)
+	puf_cro cro(
+		.challenge(sw[5:0]), // challenge[5:0] = {sel[0:2],bx[0:2}
+		.en(sw[6]),          // sw[6] is the enable 
+		.o(up)               // the latch output of the third stage is our counter trigger
+	);
+
+	always_ff@(posedge clk)
+		if((sw[6] == 1'b1))
+			cnt <= cnt + 1;
+		else
+			cnt <= 0;
+
+	always_ff@(posedge up) // if up, cnt2 <= cnt2 + 1
+			cnt2 <= cnt2 + 1;
+
+	always_latch 
+		if(cnt == 20'hFFFFF) // If count = max 
 			led <= cnt2[15:0];
 	
 endmodule
