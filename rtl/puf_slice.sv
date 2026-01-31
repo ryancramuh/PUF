99f74e879e3ab69a614911a474213980c5e0e7aa
ryancramuh <ryancramer22@gmail.com>
2026-01-30 23:47:13 -0800

    Add synthesis directives to puf_slice module
    Updated the puf_slice module to prevent optimizations and ensure correct functionality.

--- rtl/puf_slice.sv
+++ rtl/puf_slice.sv
@@ -1,5 +1,8 @@
 `timescale 1ns/1ps
 
+// avoids synthesizer optimizations
+(* dont_touch = "yes" *)
+
 module puf_slice(
 	input d0,
 	input d1,
@@ -9,13 +12,17 @@ module puf_slice(
 	output logic o,
 	output logic l_o
 ); 
-	
-	assign wire m1 = sel ? ~d1 : ~d0;
-	assign wire m2 = sel ? ~d1 : ~d0;
-	assign wire b  = bx ?  m2 :  m1;
+	// keep allows us to avoid optimizations
+	// here m1 and m2 would be optimized to form 1 signal
+	(* keep = "true" *) wire m1 = sel ? ~d1 : ~d0;
+	(* keep = "true" *) wire m2 = sel ? ~d1 : ~d0;
+	(* keep = "true" *) wire b = bx ? m2 : m1;
+
+	// o = the mux output
+	assign o = b;
 	
 	always_latch
 		if(en)
-			l_o <= b;
+			l_o <= b; // l_o = latched version of the mux output
 
 endmodule
