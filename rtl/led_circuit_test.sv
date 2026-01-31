3ee887e72b640a0c46a0c1558d8a58c7d6fa4b30
ryancramuh <ryancramer22@gmail.com>
2026-01-31 12:57:35 -0800

    Remove led_circuit_test module and update led_circuit

--- rtl/led_circuit_test.sv
+++ rtl/led_circuit_test.sv
@@ -14,43 +14,6 @@ module led_circuit
 	logic        up;   // posedge of RO     
 	logic [19:0] cnt;  // clock divider counter
 	logic [19:0] cnt2; // RO posedge counter
-	
-	puf_cro cro(
-		.challenge(sw[5:0]),
-		.en(sw[6]),
-		.o(up)
-	);
-
-	always_ff@(posedge clk)
-		if((sw[6] == 1'b1))
-			cnt <= cnt + 1;
-		else
-			cnt <= 0;
-
-	always_ff@(posedge up)
-			cnt2 <= cnt2 + 1;
-
-	always_latch 
-		if(cnt == 20'hFFFFF)
-			led <= cnt2[15:0];
-	
-endmodule
-`timescale 1ns/1ps
-
-// ensure synthesizer doens't touch this 
-(* dont_touch = "yes" *)
-
-module led_circuit
-(
-	input               clk, // global clock
-	input [6:0]         sw,  // Basys 3 switches
-	
-	output logic [15:0] led  // Basys 3 LEDs
-);
-
-	logic        up;   // posedge of RO     
-	logic [19:0] cnt;  // clock divider counter
-	logic [19:0] cnt2; // RO posedge counter
 
 	// instantiate the CRO (now as a hard macro pblock)
 	puf_cro cro(
@@ -66,10 +29,16 @@ module led_circuit
 			cnt <= 0;
 
 	always_ff@(posedge up) // if up, cnt2 <= cnt2 + 1
+		if(sw[6] == 1'b0)
+			cnt2 <= 0;
+		else if(cnt == 24'hFFFFFF)
+			cnt2 <= 0;
+		else 
 			cnt2 <= cnt2 + 1;
 
-	always_latch 
-		if(cnt == 20'hFFFFF) // If count = max 
-			led <= cnt2[15:0];
-	
+	always_ff@(posedge clk)
+		if(cnt == 24'hFFFFFF) // If count = max 
+			led <= cnt2[31:16];
+		else 
+			led <= 0;
 endmodule
